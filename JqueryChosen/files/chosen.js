5aeae9dbd52660773230d7b17ff42cd43ab7e725
Sander Brakel <s.brakel@ultraware.nl>
2017-02-08 09:48:45 +0100

    Remove List.js because already present in Mantis 2.1.0

--- JqueryChosen/files/chosen.js
+++ JqueryChosen/files/chosen.js
@@ -11,38 +11,12 @@
  */
 
 jQuery(function ($) {
-	$("select").attr("data-placeholder",SELECTION_ONE_OPTION).chosen({
-		no_results_text: NO_RESULTS,
+  $("select").attr("data-placeholder", SELECTION_ONE_OPTION).chosen({
+    no_results_text: NO_RESULTS,
     allow_single_deselect: true
   });
-  var project_menu = $("#navbar-container .dropdown-menu.dropdown-menu-right.dropdown-yellow.dropdown-caret.dropdown-close.scrollable-menu");
-  project_menu.attr("id", "project_menu_list");
-  project_menu.addClass("list");
-  $("#project_menu_list").offsetParent().attr("id", "project_menu");
-  $("#project_menu a.dropdown-toggle").attr("id", "project_menu_current");
+  $("select").removeClass("input-sm input-xs");
 
-  var current_project_name = $("#project_menu_current ").html().match(/(.*)[^<]/g)[ 1 ]
-
-  $('<input class="search" placeholder="' + current_project_name + '" id="project_menu_search" style="display:none;"/>').insertBefore("#project_menu_list");
-
-  $("#project_menu_current ").click(function () {
-    $(this).hide();
-    $("#project_menu_search").show();
-    $("#project_menu_search").focus();
-  });
-
-  $("#project_menu_search ").blur(function () {
-    $(this).hide();
-    $("#project_menu_current").show();
-  });
-
-  $("#project_menu_list li a").addClass("name");
-
-  var options = {
-    valueNames: [ 'name' ]
-  };
-
-  var projectList = new List("project_menu", options);
 })
 ;
  
